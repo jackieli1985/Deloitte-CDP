0a5583f15a288d3e241b56adf2386ce6695644c4
jackieli1985 <76101056+jackieli1985@users.noreply.github.com>
2021-03-11 17:43:57 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,44 @@
+// const path = require("path");
+const webpack = require("webpack");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const productionGzipExtensions = ["js", "css"];
+// const productionGzipExtensions = /\.(js|css|json|ttf)(\?.*)?$/i;
+
+module.exports = {
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        algorithm: "gzip",
+        test: new RegExp("\\.(" + productionGzipExtensions.join("|") + ")$"),
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 50,
+        minChunkSize: 10000
+      })
+    ],
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: false,
+          terserOptions: {
+            mangle: true, // 混淆，默认也是开的，mangle也是可以配置很多选项的
+            ecma: undefined,
+            warnings: false,
+            parse: {},
+            compress: {
+              drop_console: true,
+              drop_debugger: false,
+              pure_funcs: ["console.log"] // 移除console
+            }
+          }
+        })
+      ]
+    }
+  }
+};
